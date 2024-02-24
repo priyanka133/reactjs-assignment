c565f62f3435923acb5ae2a3f503ea4f4df61436
priYA <piyapardeshi111@gmail.com>
2024-02-24 20:36:05 +0530

    new commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Home = () => {
+  return (
+    <div className="dhiwise-navigation">
+    
+      <p className="headline">
+    
+       
+      </p>
+      <ul>
+        <li>
+          {/* <Link to="/frame">Frame</Link> */}
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default Home;
